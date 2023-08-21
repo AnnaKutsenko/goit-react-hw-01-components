458acf02f84aeac50bd80d09c78abcd89d934847
AnnaKutsenko <anisinka23@gmail.com>
2023-08-22 01:39:15 +0300

    all

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,6 @@ import { Profile } from 'components/Profile/Profile';
 import { Statistics } from 'components/Statistics/Statistics';
 import { FriendList } from 'components/FriendList/FriendList';
 import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
-
-
 import user from 'data/user.json';
 import data from 'data/data.json';
 import friends from 'data/friends.json';
